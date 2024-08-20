f13bd7ecdbc9b9d5d4d70aac417be8a36d476cd6
ushasri2645 <u.gudikandula@everest.engineering>
2024-08-20 23:17:51 +0530

    Creates context for all states and provides to child components

--- src/Context/Context.tsx
+++ src/Context/Context.tsx
@@ -1,9 +1,18 @@
-import React, { Component, Dispatch, createContext, useReducer } from "react";
+import React, {
+    Component,
+    Dispatch,
+    createContext,
+    useReducer,
+    useState,
+} from "react";
+import { data } from "../Data/Data";
+import { TItem } from "../Types/ItemType";
 
 type cartContextType = {
     cart: {
         id: number;
-        quantity: 2;
+        size: string;
+        quantity: number;
     }[];
     dispatch: Dispatch<any>;
 };
@@ -25,7 +34,9 @@ export const WishListContext = createContext<wishlistType | undefined>(
     undefined
 );
 
-export const NotificationContext = createContext<notificationType | undefined>(undefined);
+export const NotificationContext = createContext<notificationType | undefined>(
+    undefined
+);
 
 export const notificationReducer = (state: any, action: any) => {
     switch (action.type) {
@@ -38,9 +49,51 @@ export const notificationReducer = (state: any, action: any) => {
 export const cartReducer = (state: any, action: any) => {
     switch (action.type) {
         case "ADD_TO_CART":
-            return [...state, action.item];
+            const existingItem = state.findIndex(
+                (item: any) => item.id === action.item.id
+            );
+            if (existingItem !== -1) {
+                return state.map((item: any, index: number) =>
+                    index === existingItem
+                        ? {
+                              ...item,
+                              quantity: item.quantity + action.item.quantity,
+                          }
+                        : item
+                );
+            }
+            console.log(state);
+            return [
+                ...state,
+                {
+                    id: action.item.id,
+                    quantity: action.item.quantity || 1,
+                    size: action.size || "M",
+                },
+            ];
         case "REMOVE_FROM_CART":
-            return state.filter((p: any) => p.id !== action.id);
+            console.log("removing");
+            return state.filter((item: any) => item.id !== action.id);
+        case "INCREMENT":
+            return state.map((item: any) =>
+                item.id === action.id
+                    ? { ...item, quantity: item.quantity + 1 }
+                    : item
+            );
+        case "DECREMENT":
+            return state.map((item: any) =>
+                item.id === action.id
+                    ? { ...item, quantity: item.quantity - 1 }
+                    : item
+            );
+        case "SIZE":
+            return state.map((item: any) =>
+                item.id === action.id
+                    ? { ...item, size: action.size }
+                    : item
+            );
+        case "EMPTY":
+            return [];
         default:
             return state;
     }
@@ -61,21 +114,37 @@ export const wishListreducer = (state: any, action: any) => {
             return state;
     }
 };
-
+export const dataContext = createContext<any | undefined>(undefined);
 const ContextProvider = ({ children }: { children: any }) => {
     const [cart, dispatch] = useReducer(cartReducer, []);
     const [wishlist, WishListdispatch] = useReducer(wishListreducer, []);
-    const [notificationItems, Notificationdispatch] = useReducer(notificationReducer,[]);
+    const [notificationItems, Notificationdispatch] = useReducer(
+        notificationReducer,
+        []
+    );
+    const [queryResults, setQueryResults] = useState<TItem[]>([]);
+    const updateResults = (searchQuery: string) => {
+        setQueryResults(
+            data.filter((item) =>
+                item.name.toLowerCase().includes(searchQuery.toLowerCase())
+            )
+        );
+    };
+
     return (
-        <CartContext.Provider value={{ cart, dispatch }}>
-             <WishListContext.Provider value={{ wishlist, WishListdispatch }}>
-                <NotificationContext.Provider
-                    value={{ notificationItems, Notificationdispatch }}
+        <dataContext.Provider value={{ queryResults, setQueryResults }}>
+            <CartContext.Provider value={{ cart, dispatch }}>
+                <WishListContext.Provider
+                    value={{ wishlist, WishListdispatch }}
                 >
-                    {children}
-                </NotificationContext.Provider>
-            </WishListContext.Provider>
-        </CartContext.Provider>
+                    <NotificationContext.Provider
+                        value={{ notificationItems, Notificationdispatch }}
+                    >
+                        {children}
+                    </NotificationContext.Provider>
+                </WishListContext.Provider>
+            </CartContext.Provider>
+        </dataContext.Provider>
     );
 };
 
