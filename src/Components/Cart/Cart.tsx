b63410f8454aa7da82c3836194104cdaf6676a60
ushasri2645 <u.gudikandula@everest.engineering>
2024-08-21 07:24:19 +0530

    Updates Payment Functionality

--- src/Components/Cart/Cart.tsx
+++ src/Components/Cart/Cart.tsx
@@ -3,7 +3,7 @@ import { CartContext, dataContext } from "../../Context/Context";
 import { TItem } from "../../Types/ItemType";
 import styles from "./Cart.module.css";
 import { data } from "../../Data/Data";
-import { ToastContainer,toast } from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
 import Navbar from "../Navbar/Navbar";
 
 const Cart = () => {
@@ -24,6 +24,13 @@ const Cart = () => {
     const calculateItemTotal = (product: TItem, quantity: number) =>
         product.price * quantity;
 
+    const calculateItemTotalDiscount= (product:TItem,quantity:number)=>{
+        if(product.offer)
+            return (product.price * quantity)-(product.price*(product.offer/100));
+        else
+            return  product.price * quantity;
+
+    }
     const calculateCartTotal = () => {
         return cart.reduce((total, cartItem) => {
             const product = findProductById(cartItem.id);
@@ -33,96 +40,171 @@ const Cart = () => {
             return total;
         }, 0);
     };
-    const handleRemove = (id: number)=> {
-        console.log(cart);
-        const itemc= cart.find(item=>item.id===item.id)
-        console.log("cart id",itemc?.id)
-        console.log("item id", id)
-        // console.log(itemc?.id===id)
-            if (window.confirm("Do you want to remove this product from the cart?")) {
-                dispatch({ type: 'REMOVE_FROM_CART', id});
-                console.log("dispatched")
-                console.log("Updated cart:", cart);
-                toast.success("Removed From Cart")
+    const calculateCartTotalDiscount = () => {
+        return cart.reduce((total, cartItem) => {
+            const product = findProductById(cartItem.id);
+            if (product && product.offer) {
+                let val = calculateItemTotalDiscount(product, cartItem.quantity);
+                return total + val;
             }
-            else{
-                toast.warn("Remo From Cart")
-            }
+            return total;
+        }, 0);
+    };
+    const handleRemove = (id: number) => {
+        const itemc = cart.find((item) => item.id === item.id);
+        if (
+            window.confirm("Do you want to remove this product from the cart?")
+        ) {
+            dispatch({ type: "REMOVE_FROM_CART", id });
+            toast.success("Removed From Cart");
+        }
     };
     const handleDecrement = (id: number, quantity: number) => {
-        console.log(quantity)
+        console.log(quantity);
         if (quantity === 1) {
-            console.log(quantity)
-            if (window.confirm("Do you want to remove this product from the cart?")) {
-                dispatch({ type: 'REMOVE_FROM_CART', id });
+            console.log(quantity);
+            if (
+                window.confirm(
+                    "Do you want to remove this product from the cart?"
+                )
+            ) {
+                dispatch({ type: "REMOVE_FROM_CART", id });
             }
         } else {
-            console.log("came tp dece")
-            dispatch({ type: 'DECREMENT', id });
+            console.log("came tp dece");
+            dispatch({ type: "DECREMENT", id });
         }
     };
 
     return (
         <>
-        <Navbar updateResults={() => {}} /> 
-        <div className={styles.cartContainer}>
-            <ToastContainer/>
-            
-            <h1>Cart</h1>
+            <Navbar updateResults={() => {}} />
+            <ToastContainer />
             {cart.length === 0 && <p>Your cart feels very light! </p>}
-            {cart.map((cartItem) => {
-                const product = findProductById(cartItem.id);
-                if (!product) return null;
+            <h1 className={styles.title}>Cart({cart.length} items)</h1>
+            <div className={styles.cartContainer}>
+           
+                <div className={styles.topSection}>
+                    
+                    {cart.map((cartItem) => {
+                        const product = findProductById(cartItem.id);
+                        if (!product) return null;
 
-                return (
-                    <div key={cartItem.id} className={styles.cartItem}>
-                        <img
-                            src={product.image}
-                            alt={product.name}
-                            className={styles.itemImage}
-                        />
-                        <div className={styles.itemDetails}>
-                            <h2>{product.name}</h2>
-                            <p>Price: ${product.price.toFixed(2)}</p>
-                            <p>Size: {cartItem.size}</p>
-                            <p>
-                                Total: $
-                                {calculateItemTotal(
-                                    product,
-                                    cartItem.quantity
-                                ).toFixed(2)}
-                            </p>
-                            <div className={styles.quantityControls}>
-                                <button onClick={() =>handleDecrement(cartItem.id,cartItem.quantity)} >-</button><p>{cartItem.quantity}</p>
+                        return (
+                            <div key={cartItem.id} className={styles.cartItem}>
+                                <img
+                                    src={product.image}
+                                    alt={product.name}
+                                    className={styles.itemImage}
+                                />
+                                <div className={styles.itemDetails}>
+                                    <h2>{product.name}</h2>
+                                    <p>Price: ${product.price.toFixed(2)} {product.offer?<p>Discount: ${calculateItemTotalDiscount(product,cartItem.quantity).toFixed(2)}</p>:''}</p>
+
+                                    <div className={styles.quantityControls}>
+                                        <button className={styles.sizeBtn}>
+                                            {cartItem.size}
+                                        </button>
+                                        <button
+                                            className={styles.incdecBtn}
+                                            onClick={() =>
+                                                handleDecrement(
+                                                    cartItem.id,
+                                                    cartItem.quantity
+                                                )
+                                            }
+                                        >
+                                            -
+                                        </button>
+                                        <p>{cartItem.quantity}</p>
+                                        <button
+                                            className={styles.incdecBtn}
+                                            onClick={() =>
+                                                dispatch({
+                                                    type: "INCREMENT",
+                                                    id: cartItem.id,
+                                                })
+                                            }
+                                        >
+                                            +
+                                        </button>
+                                    </div>
+                                    <p>
+                                        Total: $
+                                        {calculateItemTotal(
+                                            product,
+                                            cartItem.quantity
+                                        ).toFixed(2)}
+                                    </p>
+                                    <button
+                                        className={styles.remove}
+                                        onClick={() =>
+                                            handleRemove(cartItem.id)
+                                        }
+                                    >
+                                        Remove From cart
+                                    </button>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+                <div>
+                    {cart.length > 0 ? (
+                        <div  className={styles.priceOrderSection}>
+                            <div className={styles.cartTotal}>
+                                <h2 className={styles.paymentTitle}>Payment Details</h2>
+                                <hr></hr>
+                                <div className={styles.priceDetails}>
+                                    <div className={styles.subPrice}>
+                                        <p>Price({`${cart.length} items`})</p>
+                                        <p>{calculateCartTotal().toFixed(2)}</p>
+                                    </div>
+                                   
+                                    <div className={styles.subPrice}>
+                                        <p>Discount</p>
+                                        <p className={styles.green}>{calculateCartTotalDiscount().toFixed(2)}</p>
+                                    </div>
+                                    
+                                    <div className={styles.subPrice}>
+                                        <p>Delivery Charges</p>
+                                        <p className={styles.green} >FREE</p>
+                                    </div>
+                                    <hr></hr>
+                                    <div className={styles.subPrice}>
+                                        <p>Total Amount</p>
+                                        <p>{(calculateCartTotal()-calculateCartTotalDiscount()).toFixed(2)}</p>
+                                    </div>
+                                    <hr></hr>
+                                    <p className={styles.green}>You Saved ${calculateCartTotalDiscount().toFixed(2)} on this order</p>
+                                </div>
+                                {/* <h2>Total Bill: ${calculateCartTotal().toFixed(2)}</h2> */}
+                            </div>
+                            <div className={styles.orderSection}>
+                                <button className={styles.priceBtn}>
+                                    <p>${(calculateCartTotal()-calculateCartTotalDiscount()).toFixed(2)}</p>
+                                    <p className={styles.fontSmall}>View Price Details</p>
+                                </button>
                                 <button
-                                    onClick={() =>
-                                        dispatch({
-                                            type: "INCREMENT",
-                                            id: cartItem.id,
-                                        })
-                                    }
+                                    className={styles.order}
+                                    onClick={() => {
+                                        toast.success(
+                                            `Ordered Succesfully and Total Price is: ${calculateCartTotal().toFixed(
+                                                2
+                                            )} Thanks for shopping!! Please visit again`
+                                        );
+                                        dispatch({ type: "EMPTY" });
+                                    }}
                                 >
-                                    +
+                                    Order Now
                                 </button>
                             </div>
-                            <button onClick = {()=> handleRemove(cartItem.id)}>Remove From cart</button>
                         </div>
-                    </div>
-                );
-            })}
-            
-            {cart.length>1?(
-                <>
-                <div className={styles.cartTotal}>
-                <h2>Total Bill: ${calculateCartTotal().toFixed(2)}</h2>
+                    ) : (
+                        <></>
+                    )}
+                </div>
             </div>
-                <button onClick={()=>{toast.success(`Ordered Succesfully and Total Price is: ${calculateCartTotal().toFixed(2)} Thanks for shopping!! Please visit again`);dispatch({type:"EMPTY"})}}>Order Now</button>
-                </>
-            ):(
-                <></>
-            )}
-            
-        </div>
         </>
     );
 };
