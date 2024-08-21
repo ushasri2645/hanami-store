4ea17fbfe53e652305455e9f5e95eed432e4c38d
ushasri2645 <u.gudikandula@everest.engineering>
2024-08-21 10:30:51 +0530

    Adds get request for individual product

--- Backend/routes.ts
+++ Backend/routes.ts
@@ -26,6 +26,17 @@ router.get('/items',(req,res)=>{
     res.send(items)
 })
 
+router.get('/items/:id',(req,res)=>{
+    const id = parseInt(req.params.id)
+    const item = data.find((item:TItem)=>item.id===id)
+    if(item){
+        res.send(item);
+    }
+    else{
+        res.send("Item Not Found")
+    }
+})
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
